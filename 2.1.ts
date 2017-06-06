683786555c06079aa9ca5d1a9fbadcaf04b0d1f5
deepakdeep123 <bhattacharya.deep@gmail.com>
2017-06-06 12:57:09 +0530

    Add files via upload

---
+++ 2.1.ts
@@ -0,0 +1,15 @@
+var isAvailable : boolean = true;
+
+var noofstudents : number = 100; 
+
+var idontknow =90;
+
+
+var temp4 = () => console.log('this is a arrow function');
+
+
+var temp4 = (param1 : number, param2:) => { 
+	console.log(param1+param2);
+}
+
+temp4 (1,'hello');
